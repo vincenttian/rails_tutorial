3d6fb3102ecec19eefede0fbadae84f4375cc3de
Vincent Tian <vincenttian16@gmail.com>
2014-02-25 15:41:16 -0800

    messed up front end, but beginning user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  # get "static_pages/help"
+  # get "static_pages/about"
+
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
